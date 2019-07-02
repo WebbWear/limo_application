bf6075eae6b763209b2624c1688510e6f0758804
Jeff Webb <webbwear@icloud.com>
2019-07-02 12:37:05 -0400

    got it typo

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React, { Component } from 'react';
 import { BrowserRouter as Router, Route, NavLink } from 'react-router-dom';
 
 import Landing from './components/Landing';
-import limoImg from './assets/pics/black-sprinter-limo.jpg';
+import LimoImg from './assets/pics/black-sprinter-limo.jpg';
 
 import './App.css';
 
@@ -12,7 +12,7 @@ function App() {
     <div className="App">
       <Router>
       <header className="App-header">
-        <img src="{limoImg}" alt="Palma Ceia Limo"/>
+        <img src={LimoImg} alt="Palma Ceia Limo"/>
       </header>
       </Router>
     </div>
