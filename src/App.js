cc26f5b137cd8c05228e6b8a0b36e1f2d38b3211
Jeff Webb <webbwear@icloud.com>
2019-07-13 08:16:21 -0400

    created 3 componets home, map, pics

--- src/App.js
+++ src/App.js
@@ -1,13 +1,18 @@
 import React, { Component } from 'react';
+import { BrowserRouter as Router, Link } from 'react-router-dom';
+import Route from 'react-router-dom/Route';
 import Landing from './components/Landing';
 import './App.css';
 
 class App extends Component {
     render() {
         return (
+          <Router>
             <div className="App">
-                
-            </div>
+                <Route/>
+                </div>
+          </Router>
+            
         );
     }
 }
