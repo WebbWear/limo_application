74fd549f04676f2f55556aff6dc2858c5acd5d60
Jeff Webb <webbwear@icloud.com>
2019-07-13 09:59:32 -0400

    work in progress

--- src/App.js
+++ src/App.js
@@ -1,12 +1,37 @@
 import React, { Component } from 'react';
+import { Route, Link, BrowserRouter as Router } from 'react-router-dom'
+
 import './App.css';
+
+import Landing from'./components/Landing';
+import Map from './components/Map';
+import Pics from './components/Pics';
 
 class App extends Component {
     render() {
         return (
-          <div>
-            <h1>Home</h1>
-          </div>
+          <Router>
+      <div>
+      <ul>
+        <li>
+          <Link to="/">Home</Link>
+        </li>
+        <li>
+          <Link to="/landing">Landing</Link>
+        </li>
+        <li>
+          <Link to="/map">Map</Link>
+        </li>
+        <li>
+          <Link to="/pics">Pics</Link>
+        </li>
+      </ul>
+        <Route path="/" component={App} />
+        <Route path="/landing" component={Landing} />
+        <Route path="/map" component={Map} />
+        <Route path="/pics" component={Pics} />
+      </div>
+    </Router>
             
         );
     }
